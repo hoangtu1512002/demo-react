a164b15a9b6487712f1af64b37b6d799cd7b8257
Truong Duc Ngoc <truongducngoc@codelovers.vn>
2024-06-12 18:18:03 +0700

    k

--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
    <>
     <h1>home page</h1>
+    <Link to="/admin">sdf</Link>
    </>
   )
 }
